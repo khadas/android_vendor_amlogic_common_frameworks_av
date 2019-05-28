2981f768ea89caa626d166d5a986c70d988c9323
Zhe Wang <Zhe.Wang@amlogic.com>
2019-05-28 19:56:20 +0800

    audio: optimize agc [1/3]
    PD#TV-5933
    
    Problem:
    release time and attack need change by ini
    
    Solution:
    optimize agc
    
    Verify:
    verrify by marconi
    
    Change-Id: I53f2a6460d376faccb532341b38cedcea7c08e3e
    Signed-off-by: Zhe Wang <Zhe.Wang@amlogic.com>

--- libaudioeffect/AVL/aml_agc.h
+++ libaudioeffect/AVL/aml_agc.h
@@ -21,28 +21,12 @@
 extern "C"  {
 #endif
 
-typedef struct {
-    int         agc_enable;
-    int         response_time;
-    int         release_time;
-    int         sample_max[2];
-    int         counter[2];
-    float       gain[2];
-    float       CompressionRatio;
-    int         silence_counter[2];
-    long        peak;
-    long        silence_threshold;
-    long        active_threshold;
-    float       sample_sum[2];
-    long        last_sample[2];
-    float       average_level[2];
-    float       cross_zero_num[2];
-}AmlAGC;
+void* NewAmlAGC(void);
+void DoAmlAGC(void *handle, void *buffer, int len);
+void DeleteAmlAGC(void *handle);
+int SetAmlAGC(void *handle, float peak_level, float dynamic_threshold,
+        float noise_threshold, int response_time,int release_time);
 
-AmlAGC* NewAmlAGC(float peak_level, float dynamic_theshold, float noise_threshold, int response_time, int release_time);
-void DoAmlAGC(AmlAGC *agc, void *buffer, int len);
-void DeleteAmlAGC(AmlAGC *agc);
-int SetAmlAGC(AmlAGC *agc, float peak_level, float dynamic_threshold, float noise_threshold, int response_time, int release_time);
 #ifdef __cplusplus
 }
 #endif
