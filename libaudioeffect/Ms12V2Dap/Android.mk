e3938f1a92f4cc3264d4003816aac93cda427c69
jiejing.wang <jiejing.wang@amlogic.com>
2021-04-23 20:12:57 +0800

    audio: add dap control function [1/3]
    PD#SWPL-47283
    
    Problem:
    dap function is not ready
    
    Solution:
    complete dap function
    
    Verify:
    none
    
    Change-Id: Ib7fde65b6c5d4ddc596649d2c009c3b179f48b73
    Signed-off-by: jiejing.wang <jiejing.wang@amlogic.com>

---
+++ libaudioeffect/Ms12V2Dap/Android.mk
@@ -0,0 +1,41 @@
+LOCAL_PATH := $(call my-dir)
+# MS12 DAP audio effect library
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_MODULE := libms12v2dapwrapper
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libdl \
+    libutils \
+    libamaudioutils \
+    android.hardware.audio@4.0 \
+    android.hardware.audio.common@4.0 \
+    android.hardware.audio.common@4.0-util \
+
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH) \
+    hardware/amlogic/audio/utils/ini/include \
+    hardware/amlogic/audio/utils/include \
+    hardware/libhardware/include \
+    system/media/audio/include \
+    vendor/amlogic/frameworks/av/libaudioeffect/Utility \
+    system/core/libutils/include \
+
+LOCAL_SRC_FILES := ms12v2_dap_wapper.cpp
+
+LOCAL_CFLAGS += -O2
+
+LOCAL_LDLIBS   +=  -llog
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
+LOCAL_MODULE_RELATIVE_PATH := soundfx
+
+include $(BUILD_SHARED_LIBRARY)
